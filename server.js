5f22eccd4f4a8b9f97f1f0a781adf6e0e89acfe8
Julio Graterol <juliograterolb@gmail.com>
2024-08-12 20:15:24 -0400

    Commit to test vercel deployment

--- server.js
+++ server.js
@@ -1,53 +1,52 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4 } = require('uuid')
-let data = ''
-let admins = {}
+require('dotenv').config(); // Cargar variables de entorno
 
-const port = 3000
-const hostname = 'localhost'
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { v4 } = require('uuid');
+let data = '';
+let admins = {};
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+const port = process.env.PORT || 3000;  // Usando variables de entorno
+const hostname = process.env.HOSTNAME || 'localhost';
 
-app.get ('/lab', (req, res) => {
-    res.redirect(`/lab/${v4()}`)
-})
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get('/lab/:room', (req, res) =>{
-    res.render('index', {roomId: 1})
-})
+app.get('/', (req, res) => {
+    res.redirect('/lab');
+});
 
-io.on('connection', socket=>{
-    socket.on('join-room', (roomId, userId)=>{
-        socket.join(roomId)
+app.get('/lab', (req, res) => {
+    res.redirect(`/lab/${v4()}`);
+});
+
+app.get('/lab/:room', (req, res) => {
+    res.render('index', { roomId: req.params.room });
+});
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
         if (!admins[roomId]) {
-            admins[roomId] = userId
-            console.log(admins)
+            admins[roomId] = userId;
+            console.log(admins);
         }
-        socket.broadcast.to(roomId).emit('user-connected', userId)
-       
-       
-        socket.on('share-canvas', (dataURL)=> {
-        if (admins[roomId] == userId) {
-           // if(data != dataURL){
-           //     data = dataURL
-                socket.broadcast.to(roomId).emit('update-canvas', dataURL)
-          //  }
-        }
-        })
+        socket.broadcast.to(roomId).emit('user-connected', userId);
 
+        socket.on('share-canvas', (dataURL) => {
+            if (admins[roomId] == userId) {
+                socket.broadcast.to(roomId).emit('update-canvas', dataURL);
+            }
+        });
 
-
-    socket.on('disconnect', ()=>{ 
-        socket.broadcast.to(roomId).emit('user-disconnected', userId)
-    })
-})
-})
-
+        socket.on('disconnect', () => {
+            socket.broadcast.to(roomId).emit('user-disconnected', userId);
+        });
+    });
+});
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`)
-})
+    console.log(`Server running at http://${hostname}:${port}`);
+});
